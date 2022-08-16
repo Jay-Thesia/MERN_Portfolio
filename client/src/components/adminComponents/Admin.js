d67d2ac38a4ca1cd1447a5635ffeb4dd944485e0
Jay Thesia <jayengineer.jp2410@gmail.con>
2022-08-17 00:00:01 +0530

    43. start making admin panel

--- client/src/components/adminComponents/Admin.js
+++ client/src/components/adminComponents/Admin.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./admin.css";
+import { Link } from "react-router-dom";
+import AboutAdmin from "./AboutAdmin";
+import EducationAdmin from "./EducationAdmin";
+import ExperienceAdmin from "./ExperienceAdmin";
+import ProjectAdmin from "./ProjectAdmin";
 
- const Admin = () => {
+const Admin = () => {
   return (
-    <div>hello from admin</div>
-  )
-}
+    <div className="main-container">
+      <h1 className="title">Admin forms</h1>
+      <div className="admin-center">
+        {/* about component */}
+        <h3 className="admin-title">About Component</h3>
+        <AboutAdmin />
+        {/* end of about componet */}
 
-export default Admin
+        <br />
+        <hr />
+        {/* Education component */}
+        <h3 className="admin-title">Education Component</h3>
+        <EducationAdmin/>
+        {/* End of education componet */}
+
+        <br />
+        <hr />
+        {/* Project component */}
+        <h3 className="admin-title">Project Component</h3>
+        <ProjectAdmin/>
+        {/* End of project component */}
+
+        <br />
+        <hr />
+        {/* Experience component */}
+        <h3 className="admin-title">Experience Component</h3>
+        <ExperienceAdmin/>
+        {/* End of experience component */}
+
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
