65be2a9198eb83b1f3e6b2ae5df39511e7d0e72d
Jay Thesia <jayengineer.jp2410@gmail.con>
2022-08-24 23:27:47 +0530

    44. about admin componets html

--- client/src/components/adminComponents/AboutAdmin.js
+++ client/src/components/adminComponents/AboutAdmin.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
- const AboutAdmin = () => {
+const AboutAdmin = () => {
   return (
-    <div>hello from aboutAdmin</div>
-  )
-}
+    <div className="same-component">
+      <div className="same-form">
+        <form>
+          <h4>About Component</h4>
+          <label htmlFor="text">About :</label>
+          <textarea name="textarea" cols="30" rows="3"></textarea>
+          <button type="submit">Update</button>
+        </form>
+      </div>
 
-export default AboutAdmin
+      <div className="same-item">
+        <div className="about-info">
+          <div className="icons">
+            <Link to={"/"}>
+              {" "}
+              <i
+                style={{ color: "black", fontSize: "20px" }}
+                className="fa-solid fa-edit"
+              ></i>{" "}
+            </Link>
+            <i
+              style={{ color: "black", fontSize: "20px" }}
+              className="fa-solid fa-trash"
+            ></i>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam
+            iusto quod blanditiis amet odio eos commodi similique sint, soluta
+            sunt quisquam magni tempora quas quasi explicabo omnis cumque eius!
+            Accusamus.
+          </p>
+        </div>
+      </div>
+
+      <h3 className="item-delete-tab">item deleted</h3>
+    </div>
+  );
+};
+
+export default AboutAdmin;
