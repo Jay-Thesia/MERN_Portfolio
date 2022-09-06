de22fef91eeea9b4134738e7aba96a11ce2607cf
Jay Thesia <jayengineer.jp2410@gmail.con>
2022-09-06 21:59:54 +0530

    54. edit experience component and completed all the update Link

--- client/src/components/editComponents/EditExperience.js
+++ client/src/components/editComponents/EditExperience.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
- const EditExperience = () => {
+const EditExperience = () => {
   return (
-    <div>hello form editExperience</div>
-  )
-}
+    <div className="edit">
+      <div className="main-container">
+        <div className="same-component">
+          <div className="same-form">
+            <form>
+              <h3>Update Experience</h3>
 
-export default EditExperience
+              <label htmlFor="text">Experience :</label>
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+
+              <div className="btns">
+                <button className="update-btn">Update Experience</button>
+                <Link to="/admin">
+                  <button className="cancel-btn">Cancel</button>
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditExperience;
