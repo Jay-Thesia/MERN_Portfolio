38aac05f63e08f41a4f946fe6cab0adeab4b48a4
Jay Thesia <jayengineer.jp2410@gmail.con>
2022-09-06 21:18:57 +0530

    52. edit education components

--- client/src/components/editComponents/EditEducation.js
+++ client/src/components/editComponents/EditEducation.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./edit.css";
 
- const EditEducation = () => {
+const EditEducation = () => {
   return (
-    <div>hello form editEducation</div>
-  )
-}
+    <div className="edit">
+      <div className="main-container">
+        <div className="same-component">
+          <div className="same-form">
+            <form>
+              <h3 className="updated">Updated Education</h3>
+              <label htmlFor="text">Specialization :</label>
+              <input type="text" placeholder="Course Name" require />
 
-export default EditEducation
+              <label htmlFor="date">Start Date : </label>
+              <input type="date" require />
+
+              <label htmlFor="date">End Date : </label>
+              <input type="date" require />
+
+              <label htmlFor="text">Organization :</label>
+              <input type="text" placeholder="College Name" require />
+
+              <label htmlFor="number">Grades :</label>
+              <input type="number" placeholder="GPA" require />
+
+              <div className="btns">
+                <button type="submit" className="update-btn">
+                  Update Edu
+                </button>
+                <Link to="/admin"><button className="cancel-btn">Cancel</button></Link> 
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditEducation;
