6bf426dfd6ab7264bc87b595dc14921ba0fe65c8
Jay Thesia <jayengineer.jp2410@gmail.con>
2022-09-09 16:05:07 +0530

    57. About component fetch data from DB

--- client/src/components/context/GlobalContext.js
+++ client/src/components/context/GlobalContext.js
@@ -1,12 +1,32 @@
-import axios from 'axios'
-import React from 'react'
-import { createContext } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-export const DataContext=createContext();
-export const DataProvider=({children})=>{
-    return(
-        <DataContext.Provider value='hello form context'>
-            {children}
-        </DataContext.Provider>
-    );
-}
+import { createContext } from "react";
+
+export const DataContext = createContext();
+export const DataProvider = ({ children }) => {
+  const [aboutValue, setAbout] = useState([]);
+
+  //fetching data
+  const fetchData = async () => {
+    const res1 = await axios.get("/about");
+    // console.log(res1.data);
+    setAbout(res1.data);
+  };
+
+  //use effect to take the data from backend dymaically and render it
+  useEffect(() => {
+    try {
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  //create state
+  const state = {
+    about: [aboutValue, setAbout]
+  };
+
+  return <DataContext.Provider value={state}>{children}</DataContext.Provider>;
+};
