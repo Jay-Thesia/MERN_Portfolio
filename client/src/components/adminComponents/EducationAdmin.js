eadc9b6004ea52dfaa2ca484bc9db8557d0ab714
Jay Thesia <jayengineer.jp2410@gmail.con>
2022-09-14 00:42:52 +0530

    64. Education admin add and delete

--- client/src/components/adminComponents/EducationAdmin.js
+++ client/src/components/adminComponents/EducationAdmin.js
@@ -1,18 +1,73 @@
-import React,{useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
 import "./admin.css";
 
 const EducationAdmin = () => {
+  const [eduValue, setEducation] = useState("");
+  const [eduData, setEduData] = useState([]);
+  const [message, setMessage] = useState("");
+  const [msgCondition, setMsgCondition] = useState(false);
 
-  const[eduValue,setEducation]=useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("/education");
+      setEduData(res.data);
+    };
+    try {
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+    //aboutdata in array use to reload a page
+  }, []);
+
+
+  const onChangeEducation=(e)=>{
+    setEducation(e.target.value);
+  }
+  //submit/add education
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const postEducation={
+      eduValue
+    }
+
+    setEducation('');
+    axios.post('/education',postEducation)
+    .then(res=>{
+      console.log("added");
+    }).catch(err=>console.log(err))
+  };
+
+
+  //delete the entry
+  const deleteEducation = (id) => {
+    axios
+      .delete(`/education/${id}`)
+      .then((res) => {
+        setMsgCondition(true);
+        setMessage(`${res.data.msg}`);
+
+        setTimeout(() => {
+          setMessage("");
+          setMsgCondition(false);
+        }, 2000);
+      })
+      .catch((err) => console.log(err));
+
+    //delete from ui
+    const eduDeleteFilter = eduData.filter((item) => item._id != id);
+    setEduData(eduDeleteFilter);
+  };
 
   return (
     <div className="same-component">
       <div className="same-form">
-        
-      <h3 className="item-delete-tab">The message</h3>
         <h3 className="eduAdmin">Education Component:</h3>
-        <form className="eduForm">
+        <form className="eduForm" onSubmit={handleSubmit}>
           {/* <label htmlFor="text">Specialization & Date :</label>
           <input type="text" placeholder="Course Name & date(Ex.2019-23)" require="true" />
 
@@ -21,6 +76,8 @@ const EducationAdmin = () => {
 
           <label htmlFor="number">Grades :</label>
           <input type="number" placeholder="GPA" require="true" /> */}
+          <label htmlFor="">Education :</label>
+          <textarea name="" id="" cols="25" rows="8" value={eduValue} onChange={onChangeEducation}></textarea>
 
           <br />
           <button type="submit" className="updateButton">
@@ -30,35 +87,34 @@ const EducationAdmin = () => {
       </div>
 
       <div className="same-item">
-        <div className="about-info">
-          {/* single education */}
+        <h3 className={setMsgCondition?"new-delete item-delete-tab":"item-delete-tab"}>{message}</h3>
+        {eduData.map((item) => (
+          <div className="about-info" key={item._id}>
+            {/* single education */}
 
-          <div className="same-admin">
-            <div className="icons">
-              <Link to={"/editEducation"}>
+            <div className="same-admin">
+              <div className="icons">
+                <Link to={"/editEducation"}>
+                  <i
+                    style={{ color: "green", fontSize: "20px" }}
+                    className="fa-solid fa-edit"
+                  ></i>
+                </Link>
                 <i
-                  style={{ color: "green", fontSize: "20px" }}
-                  className="fa-solid fa-edit"
+                  style={{ color: "red", fontSize: "20px" }}
+                  className="fa-solid fa-trash"
+                  onClick={() => deleteEducation(item._id)}
                 ></i>
-              </Link>
-              <i
-                style={{ color: "red", fontSize: "20px" }}
-                className="fa-solid fa-trash"
-              ></i>
+              </div>
+
+              <br />
+
+              <div className="single-education">
+                <p>{item.eduValue}</p>
+              </div>
             </div>
-
-            <br />
-
-            <div className="single-education">
-              <p><b>Information & Technology(2019-2023)</b> </p>
-              <p>Lukhdhirji Engineering College,Morbi</p>
-              <p>CGPA: 9.17/10</p>
-            </div>
-
-           
           </div>
-          
-        </div>
+        ))}
       </div>
     </div>
   );
