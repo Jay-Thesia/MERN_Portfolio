4934734308133633732ee3376e51b8476e82d63e
Jay Thesia <jayengineer.jp2410@gmail.con>
2022-09-18 21:55:19 +0530

    basic changes

--- controllers/uploadCtrl.js
+++ controllers/uploadCtrl.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_SECRET,
+    api_secret: process.env.CLOUD_API_SECRET,
   });
   
 exports.postImage=(req, res) => {
