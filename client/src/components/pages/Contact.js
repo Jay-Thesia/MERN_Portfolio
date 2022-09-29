bdef13aa4ca4e7817e8dc7a29f0cba8d6b75e44f
Jay Thesia <jayengineer.jp2410@gmail.con>
2022-09-29 23:15:15 +0530

    76. contact form submit

--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -1,8 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
 import contact from "../../images/ContactBanner.gif";
+import send from "../../images/Send_Mail.gif";
+import axios from "axios";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [banner, setBanner] = useState("");
+  const [bool, setBool] = useState(false);
+  const [showButton, setShowButton] = useState(true);
+
+  //handle input
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+    // console.log(name);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    // console.log(email);
+  };
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+    // console.log(message);
+  };
+
+  //form submit
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+
+    let data = {
+      name,
+      email,
+      message,
+    };
+
+    setBool(true);
+
+    axios
+      .post(`/contact`, data)
+      .then((res) => {
+        setBanner(res.data.msg);
+        setBool(false);
+
+        setTimeout(() => {
+          setBanner("");
+        }, 2000);
+
+        setName("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response.data);
+        }
+      });
+  };
+
+  //button
+  const onClickSend = () => {
+
+    // setShowButton(false);
+
+    setTimeout(() => {
+      setShowButton(false);
+    }, 100);
+
+    <div className="">
+       {showButton ?null  : <button type="submit" onClick={onClickSend}>
+                  {/* <i className="fa fa-paper-plane"></i> */}
+                  Send
+                </button>}
+    </div>
+  //     {/* <i className="fa fa-paper-plane"></i> */}
+
+    setTimeout(() => {
+      setShowButton(true)
+    }, 5000);
+  };
+
   return (
     <div className="main-container" id="Contact">
       <div className="contactForm">
@@ -10,13 +91,25 @@ const Contact = () => {
 
         <div className="contactFrom-center">
           <div className="contact_form">
-            <form>
-              <p><big> <b>Message</b></big></p>
+            <form onSubmit={formSubmit}>
+              <p>{banner}</p>
               <label htmlFor="name">Name: </label>
-              <input type="text" placeholder="Enter your name" required />
+              <input
+                type="text"
+                placeholder="Enter your name"
+                required
+                value={name}
+                onChange={handleNameChange}
+              />
 
               <label htmlFor="email">Email: </label>
-              <input type="email" placeholder="Enter your mail ID" required />
+              <input
+                type="email"
+                placeholder="Enter your mail ID"
+                required
+                value={email}
+                onChange={handleEmailChange}
+              />
 
               <label htmlFor="message">Message: </label>
               <textarea
@@ -25,10 +118,28 @@ const Contact = () => {
                 cols="20"
                 rows="7"
                 placeholder="Enter your message"
-              ></textarea>
+                value={message}
+                onChange={handleMessageChange}
+              />
 
-              <div className="sent-btn">
-                <button type="submit"><i className="fa fa-paper-plane"></i></button>
+              <div className="sent-btn" onClick={onClickSend}>
+                {showButton && <button type="submit">Send</button>}
+
+                {/* <i className="fa fa-paper-plane"></i> */}
+              </div>
+
+              <div style={{ textAlign: "center", margin: "10px" }}>
+                {bool ? (
+                  <b className="load">
+                    <img
+                      src={send}
+                      alt=""
+                      style={{ width: "100px", height: "100px" }}
+                    />
+                  </b>
+                ) : (
+                  ""
+                )}
               </div>
             </form>
           </div>
--- controllers/contactCtrl.js
+++ controllers/contactCtrl.js
@@ -1,52 +1,60 @@
 const nodemailer = require("nodemailer");
+const {google}=require("googleapis")
+const OAuth2=google.OAuth2;
 
-exports.getMail=(req, res) => {
-    try {
-      res.send("contact");
-    } catch (error) {
-      return res.status(500).json({ msg: "server problem" });
-    }
-  };
+exports.getMail = (req, res) => {
+  try {
+    res.send("contacted");
+  } catch (error) {
+    return res.status(500).json({ msg: "server problem" });
+  }
+};
 
-exports.sendMail=(req, res) => {
-    //smtp simple mail transfer procol use for transporting mail every email providors support it
-  
-    let smtpTransport = nodemailer.createTransport({
-      service: "Gmail",
-  
-      //the port of connect
-      port: 465,
-  
-      //for authenticate
-      auth: {
+exports.sendMail = (req, res) => {
+  //smtp simple mail transfer procol use for transporting mail every email providors support it
+
+  let data = req.body;
+  let smtpTransport = nodemailer.createTransport({
+    service: "Gmail",
+
+    //the port of connect
+    port: 465,
+
+    //for authenticate
+    auth: {
+        type:"OAuth2",
         user: "jayengineer.jp2410@gmail.com",
-        pass: "jay9974967074",
-      },
-    });
-  
-    let mailOptions = {
-      from: data.email,
-      to: "jayengineer.jp2410@gmail.com",
-      subject: "Message from   ${data.name}",
-      html: `
+        // password: "jay9974967074",
+        clientId:process.env.CLIENT_ID,
+        clientSecret:process.env.CLIENT_SECRET,
+        refreshToken:process.env.CLIENT_REFRESH_TOKEN
+    
+        
+    },
+  });
+
+  let mailOptions = {
+    from:data.email,
+    to: "jayengineer.jp2410@gmail.com",
+    subject: `Message from ${data.name}`,
+    html: `
             <h3>Information</h3>
             <ul>
                 <li>Name:${data.name}</li>
-                <li>Name:${data.email}</li>
+                <li>Email:${data.email}</li>
             </ul>
     
             <h3>Message</h3>
             <p>${data.message}</p>
         `,
-    };
-  
-    smtpTransport.sendMail(mailOptions, (err, response) => {
-      try {
-        if (err) return res.status(400).json({ msg: err.message });
-        else return res.status(200).json({ msg: "Mail is send" });
-        
-      } catch (error) {
-        return res.status(500).json({ msg: error });
-      }
-    });
   };
+
+  smtpTransport.sendMail(mailOptions, (err, response) => {
+    try {
+      if (err) return res.status(400).json({ msg: err.message });
+      else return res.status(200).json({ msg: "Mail is send" });
+    } catch (error) {
+      return res.status(500).json({ msg: error });
+    }
+  });
+};
