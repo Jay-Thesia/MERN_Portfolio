5a66a6e2d5744e7dc5ccb68bc66b999b41142381
Jay Thesia <jayengineer.jp2410@gmail.con>
2022-09-30 22:05:20 +0530

    revert to bdef13aa4ca4e7817e8dc7a29f0cba8d6b75e44f

--- client/src/components/adminComponents/ProjectAdmin.js
+++ client/src/components/adminComponents/ProjectAdmin.js
@@ -149,8 +149,8 @@ const ProjectAdmin = () => {
             onChange={handleChangeInput}
           />
 
-          <label htmlFor="link">Github Link :</label>
-          <input name="link" id="link" onChange={handleChangeInput}/>
+          {/* <label htmlFor="link">Github Link :</label>
+          <input name="link" id="link" /> */}
 
           <div className="upload">
             <input
@@ -175,6 +175,7 @@ const ProjectAdmin = () => {
       <div className="same-item">
         <div className="about-info">
           <div className="projects-admin">
+            
             <div className="icons">
               <Link to={"/editProject"}>
                 <i
