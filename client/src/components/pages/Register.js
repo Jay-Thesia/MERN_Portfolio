cdf76c827e839494f461ee17f740f13b41c4fb0e
Jay Thesia <jayengineer.jp2410@gmail.con>
2022-10-02 17:05:50 +0530

    79. Register component functionality

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [user, setUser] = useState({ username: "", email: "", password: "" });
+  const [err, setErr] = useState("");
+
+  //onChangeInput
+
+  const onChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+    setErr("");
+  };
+
+  //register submit
+
+  const registerSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(`/user/register`, {
+
+        username:user.username,
+        email:user.email,
+        password:user.password
+      });
+
+      setUser({ username: "", email: "", password: "" })
+      setErr(res.data.msg)
+    } catch (error) {
+      error.response.data.msg && setErr(error.response.data.msg)
+    }
+  };
   return (
     <>
       <div className="login">
         <div className="main-container">
-          <h1 className="title">Login for admin</h1>
+          <h1 className="title">Register admin</h1>
 
           <div className="login-center">
-            <form action="">
-              <p>You edited it</p>
+            <form onSubmit={registerSubmit}>
+              <p>{err}</p>
               <label htmlFor="name">Name :</label>
               <input
-                type="email"
+                type="text"
                 placeholder="Enter your Name"
-                name="email"
+                name="username"
                 required
+                value={user.username}
+                onChange={onChangeInput}
               />
               <label htmlFor="email">Email :</label>
               <input
@@ -24,6 +57,8 @@ const Register = () => {
                 placeholder="Enter your Email"
                 name="email"
                 required
+                value={user.email}
+                onChange={onChangeInput}
               />
 
               <label htmlFor="password">Password :</label>
@@ -32,22 +67,22 @@ const Register = () => {
                 placeholder="Enter your Password"
                 name="password"
                 required
+                value={user.password}
+                onChange={onChangeInput}
               />
 
               <div className="login-btn">
-                <button id="login" type="submit">
+                <button id="register" type="submit">
                   Register
                 </button>
                 <Link to="/">
-                  <button>Back to home</button>
+                  <button>Home</button>
                 </Link>
               </div>
             </form>
           </div>
         </div>
       </div>
-
-    
     </>
   );
 };
