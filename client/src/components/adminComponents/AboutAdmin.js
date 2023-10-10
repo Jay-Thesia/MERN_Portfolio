3c032b02de44bc923a63057148e6bbd3a892665e
Jay Thesia <jayengineer.jp2410@gmail.com>
2023-10-10 09:32:51 +0530

    Update AboutAdmin.js

--- client/src/components/adminComponents/AboutAdmin.js
+++ client/src/components/adminComponents/AboutAdmin.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import e from "cors";
+// import e from "cors";
 
 const AboutAdmin = () => {
   const [aboutValue, setAbout] = useState("");
